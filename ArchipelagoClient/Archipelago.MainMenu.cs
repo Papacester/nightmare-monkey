8cf55f49e2354720cfe6f3184a2cf37425ccac5e
Papacester <106293907+Papacester@users.noreply.github.com>
2026-01-19 17:15:40 -0700

    Refactor MainMenuScreen patches for Narcopelago

--- ArchipelagoClient/Archipelago.MainMenu.cs
+++ ArchipelagoClient/Archipelago.MainMenu.cs
@@ -1,10 +1,11 @@
-using Archipelago.UI;
-using MelonLoader;
 using HarmonyLib;
+using MelonLoader;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Archipelago
+
+namespace Narcopelago
 {
     public static class Schedule1PanelManager
     {
@@ -13,46 +14,25 @@ namespace Archipelago
     }
 
 
-    [HarmonyPatch(typeof(ScheduleOne.UI.MainMenu.MainMenuScreen), "Awake")]
+    [HarmonyPatch(typeof(Il2CppScheduleOne.UI.MainMenu.MainMenuScreen), "Awake")]
     public class MainMenuScreen_Awake_Patch
     {
-        static void Postfix(ScheduleOne.UI.MainMenu.MainMenuScreen __instance)
+        static void Postfix(Il2CppScheduleOne.UI.MainMenu.MainMenuScreen __instance)
         {
+            MelonLogger.Msg("Scene during MainMenuScreen.Awake: " + SceneManager.GetActiveScene().name);
+
+            // Only create panel in the real main menu
+            if (SceneManager.GetActiveScene().name != "Menu")
+                return;
+
             if (Schedule1PanelManager.PanelInstance != null)
                 return;
 
-            GameObject panel = Schedule1PanelBuilder.CreatePanel(__instance.transform);
+            var canvas = GameObject.FindObjectOfType<Canvas>();
+            GameObject panel = NarcopelagoUI.CreatePanel(canvas.transform);
+
             Schedule1PanelManager.PanelInstance = panel;
-            panel.SetActive(false);
-        }
-    }
-
-
-    [HarmonyPatch(typeof(ScheduleOne.UI.MainMenu.MainMenuScreen), "Open")]
-    public class MainMenuScreen_Open_Patch
-    {
-        static void Postfix(ScheduleOne.UI.MainMenu.MainMenuScreen __instance)
-        {
-            if (Schedule1PanelManager.PanelInstance != null)
-            {
-                Schedule1PanelManager.PanelInstance.SetActive(true);
-                Schedule1PanelManager.PanelInstance.transform.SetAsLastSibling();
-                MelonLogger.Msg("Reactivating Schedule1Panel");
-            }
-        }
-    }
-
-
-    [HarmonyPatch(typeof(ScheduleOne.UI.MainMenu.MainMenuScreen), "Close")]
-    public class MainMenuScreen_Close_Patch
-    {
-        static void Postfix(ScheduleOne.UI.MainMenu.MainMenuScreen __instance)
-        {
-            if (Schedule1PanelManager.PanelInstance != null)
-            {
-                Schedule1PanelManager.PanelInstance.SetActive(false);
-                MelonLogger.Msg("Hiding Schedule1Panel");
-            }
+            panel.SetActive(true);
         }
     }
 }
